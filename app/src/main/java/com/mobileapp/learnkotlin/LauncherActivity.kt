c8a4104a46852863712cafeca24fb611f1f3cc7d
danielx6421 <a.danielpf21@gmail.com>
2019-12-13 11:44:45 +0530

    Integrate Data Binding

--- app/src/main/java/com/mobileapp/learnkotlin/LauncherActivity.kt
+++ app/src/main/java/com/mobileapp/learnkotlin/LauncherActivity.kt
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import codelabs.constraintlayout.ColorMyViewsAppActivity
 import codelabs.diceapp.DiceAppActivity
+import com.mobileapp.learnkotlin.databinding.ActivityLauncherBinding
 
 class LauncherActivity : AppCompatActivity() {
 
     lateinit var diceApp : Button
     lateinit var colorMyViewApp : Button
+    lateinit var binding : ActivityLauncherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launcher)
-        diceApp = findViewById(R.id.dice_app_launch_button)
-        colorMyViewApp = findViewById(R.id.color_my_view_app_launch_button)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_launcher)
+        diceApp = binding.diceAppLaunchButton
+        colorMyViewApp = binding.colorMyViewAppLaunchButton
     }
 
     fun navigateToNextPage(view: View) {
