2a086445433b4cf7cad2f95cc3a8150fe91aac51
daniedev <a.danielpf21@gmail.com>
2020-04-24 11:09:43 +0530

    Up Button & Options Menu

--- app/src/main/java/codelabs/androidtrivia/AndroidTriviaActivity.kt
+++ app/src/main/java/codelabs/androidtrivia/AndroidTriviaActivity.kt
@@ -3,16 +3,25 @@ package codelabs.androidtrivia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mobileapp.learnkotlin.R
 import com.mobileapp.learnkotlin.databinding.ActivityAndroidTriviaBinding
 
 class AndroidTriviaActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAndroidTriviaBinding
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_android_trivia)
+        navController = this.findNavController(R.id.triva_nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
 }
--- app/src/main/java/codelabs/androidtrivia/TitleFragment.kt
+++ app/src/main/java/codelabs/androidtrivia/TitleFragment.kt
@@ -2,12 +2,11 @@ package codelabs.androidtrivia
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mobileapp.learnkotlin.R
 import com.mobileapp.learnkotlin.databinding.FragmentTitleBinding
 
@@ -16,16 +15,35 @@ import com.mobileapp.learnkotlin.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
-    lateinit var binding : FragmentTitleBinding
+    lateinit var binding: FragmentTitleBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,R.layout.fragment_title,container,false)
-        binding.triviaPlayButton.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
-        return binding.root
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+        binding.triviaPlayButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.trivia_options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
 
 
 }
