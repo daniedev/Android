e1e54efa68dad3901dfa021273b5f7a04051af8f
daniedev <a.danielpf21@gmail.com>
2020-04-24 12:27:45 +0530

    Navigation Drawer

--- app/src/main/java/codelabs/androidtrivia/AndroidTriviaActivity.kt
+++ app/src/main/java/codelabs/androidtrivia/AndroidTriviaActivity.kt
@@ -3,6 +3,7 @@ package codelabs.androidtrivia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -13,15 +14,18 @@ class AndroidTriviaActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAndroidTriviaBinding
     lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_android_trivia)
         navController = this.findNavController(R.id.triva_nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
