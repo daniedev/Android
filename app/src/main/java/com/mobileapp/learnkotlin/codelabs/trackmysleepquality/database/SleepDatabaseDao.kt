05fb8735ffe14fffdbda7b722644cd52840226d1
daniedev <a.danielpf21@gmail.com>
2020-05-23 13:45:27 +0530

    Creating a Room DB - Track My Sleep Quality

--- app/src/main/java/com/mobileapp/learnkotlin/codelabs/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/mobileapp/learnkotlin/codelabs/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -1,6 +1,29 @@
 package com.mobileapp.learnkotlin.codelabs.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE from daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+}
